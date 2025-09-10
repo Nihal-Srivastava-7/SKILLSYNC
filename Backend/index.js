9356c43ec8ef90b60e0bade99efcb26478f25b01
Nihal Srivastava <160469047+Nihal-Srivastava-7@users.noreply.github.com>
2025-09-10 13:32:47 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -24,12 +24,7 @@ app.use(
 );
 
 // Preflight support 
-app.options("/*", (req, res) => {
-  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  res.sendStatus(200);
-});
+
 
 
 app.use(express.json());
